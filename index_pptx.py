3a32285000bd6fd24bcd3f197be8af4e09f536d1
Tim Moran <tim@t-moran.com>
2018-01-06 12:17:13 +0000

    Do thumbnails in a temporary directory

--- index_pptx.py
+++ index_pptx.py
@@ -1,6 +1,7 @@
 import os.path
 import subprocess
 import sys
+import tempfile
 import xml.etree.ElementTree as ET
 import zipfile
 
@@ -13,55 +14,57 @@ def copy_in_zip(zip_handle, from_path, to_path):
         write_slide.write(to_copy)
     return to_copy
 
-def generate_thumbs(powerpoint_file):
+def index_file(powerpoint_file):
     original_filename = os.path.realpath(powerpoint_file)
-    
+
     session = database.Session()
     doc = database.Document()
     doc.path = original_filename
     session.add(doc)
 
-    subprocess.run(['cp', powerpoint_file, 'trashed.pptx'])
-    powerpoint_file = 'trashed.pptx'
-    with zipfile.ZipFile(powerpoint_file) as pptx_zip:
-        slide_count = len([i.filename for i in pptx_zip.infolist() if i.filename.startswith('ppt/slides/') and not i.filename.startswith('ppt/slides/_rels')])
+    with tempfile.TemporaryDirectory() as tempdir:
+        powerpoint_file = os.path.join(tempdir, 'trashed.pptx')
+        thumb_name = os.path.join(tempdir, 'trashed.pptx.png')
 
-    print(slide_count)
+        subprocess.run(['cp', original_filename, powerpoint_file])
 
-    for slide_number in range(1, slide_count+1):
-        path = 'ppt/slides/slide' + str(slide_number) + '.xml'
-        rels_path = 'ppt/slides/_rels/slide' + str(slide_number) + '.xml.rels'
+        with zipfile.ZipFile(powerpoint_file) as pptx_zip:
+            slide_count = len([i.filename for i in pptx_zip.infolist() if i.filename.startswith('ppt/slides/') and not i.filename.startswith('ppt/slides/_rels')])
 
-        print(path)
+        print(slide_count)
 
-        with zipfile.ZipFile(powerpoint_file, 'a') as pptx_zip:
-            print("slides:")
-            print([i.filename for i in pptx_zip.infolist() if i.filename.startswith('ppt/slides/') and not i.filename.startswith('ppt/slides/_rels')])
-            if slide_number > 1:
-                xml_content = copy_in_zip(pptx_zip, path, 'ppt/slides/slide1.xml')
-                copy_in_zip(pptx_zip, rels_path, 'ppt/slides/_rels/slide1.xml.rels')
-            else:
-                with pptx_zip.open(path) as read_slide:
-                    xml_content = read_slide.read()
+        for slide_number in range(1, slide_count+1):
+            path = 'ppt/slides/slide' + str(slide_number) + '.xml'
+            rels_path = 'ppt/slides/_rels/slide' + str(slide_number) + '.xml.rels'
 
-        thumb_name = original_filename + '_slide' + str(slide_number) + '.png'
-        subprocess.run(['qlmanage', '-t', '-s', '400', '-o', '.', powerpoint_file])
-        
-        thumbnail = database.Slide()
-        thumbnail.document = doc
-        thumbnail.slide = slide_number
-        
-        text_content = scrape_text_content(xml_content)
+            print(path)
 
-        slide_content = database.SlideContent()
-        slide_content.content = text_content
-        slide_content.slide = thumbnail
-        session.add(slide_content)
+            with zipfile.ZipFile(powerpoint_file, 'a') as pptx_zip:
+                print("slides:")
+                print([i.filename for i in pptx_zip.infolist() if i.filename.startswith('ppt/slides/') and not i.filename.startswith('ppt/slides/_rels')])
+                if slide_number > 1:
+                    xml_content = copy_in_zip(pptx_zip, path, 'ppt/slides/slide1.xml')
+                    copy_in_zip(pptx_zip, rels_path, 'ppt/slides/_rels/slide1.xml.rels')
+                else:
+                    with pptx_zip.open(path) as read_slide:
+                        xml_content = read_slide.read()
 
+            subprocess.run(['qlmanage', '-t', '-s', '400', '-o', tempdir, powerpoint_file])
 
-        with open('trashed.pptx.png', 'rb') as png_file:
-            thumbnail.thumnail_png = png_file.read()
-        session.add(thumbnail)
+            thumbnail = database.Slide()
+            thumbnail.document = doc
+            thumbnail.slide = slide_number
+
+            text_content = scrape_text_content(xml_content)
+
+            slide_content = database.SlideContent()
+            slide_content.content = text_content
+            slide_content.slide = thumbnail
+            session.add(slide_content)
+
+            with open(thumb_name, 'rb') as png_file:
+                thumbnail.thumnail_png = png_file.read()
+            session.add(thumbnail)
 
     session.commit()
 
@@ -71,6 +74,5 @@ def scrape_text_content(xml_content):
     return '\n'.join([text_elem.text for text_elem in tree.iter('{http://schemas.openxmlformats.org/drawingml/2006/main}t')])
 
 if __name__ == '__main__':
-    filename = sys.argv[1]
-    generate_thumbs(filename)
+    index_file(sys.argv[1])
 
