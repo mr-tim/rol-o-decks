36103c3d1804fe112818622c95da291b23017e30
Tim Moran <tim@t-moran.com>
2019-01-10 08:23:11 +0000

    Basic model and messages

--- elm-ui/src/Main.elm
+++ elm-ui/src/Main.elm
@@ -1,16 +1,11 @@
 module Main exposing (..)
 
 import Browser
-import Browser.Dom as Dom
 import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
-import Html.Keyed as Keyed
-import Html.Lazy exposing (lazy, lazy2)
-import Task
 
-main : Program (Maybe Model) Model Msg
+main : Program () Model Msg
 main =
+    -- TODO: Switch to Browser.application
     Browser.document
         { init = init
         , view = \model -> {
@@ -22,25 +17,48 @@ main =
         }
 
 type alias Model =
-    {}
+    { searchTerm : String
+    , searchResults : List SearchResult
+    }
+
+type alias SearchResult =
+    { path : String
+    , slide : Int
+    , thumbnailBase64 : String
+    , match : SearchMatch
+    }
+
+type alias SearchMatch =
+    { text : String
+    , start : Int
+    , end : Int
+    }
 
 emptyModel : Model
 emptyModel =
-    {}
+    { searchTerm = ""
+    , searchResults = []
+    }
 
-init : Maybe Model -> ( Model, Cmd Msg )
-init maybeModel =
-  ( Maybe.withDefault emptyModel maybeModel
+init : () -> ( Model, Cmd Msg )
+init _ =
+  ( emptyModel
   , Cmd.none
   )
 
 type Msg
     = NoOp
+    | UpdatedSearchTerm
+    | FetchedSearchResults
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
+            ( model, Cmd.none )
+        UpdatedSearchTerm ->
+            ( model, Cmd.none )
+        FetchedSearchResults ->
             ( model, Cmd.none )
 
 view : Model -> Html Msg
